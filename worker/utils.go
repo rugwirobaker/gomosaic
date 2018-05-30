ca64c0a46181eb38999302cbe9782a7d81887638
rugwirobaker <rugwirobaker@gmail.com>
2018-05-30 23:58:17 +0200

    CREATED utils file

---
+++ worker/utils.go
@@ -0,0 +1,16 @@
+package worker
+
+import "image"
+
+func avgColor(img image.Image) [3]float64 {
+	bounds := img.Bounds()
+	r, g, b := 0.0, 0.0, 0.0
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r1, g1, b1, _ := img.At(x, y).RGBA()
+			r, g, b = r+float64(r1), g+float64(g1), b+float64(b1)
+		}
+	}
+	totalPixels := float64(bounds.Max.X * bounds.Max.Y)
+	return [3]float64{r / totalPixels, g / totalPixels, b / totalPixels}
+}
