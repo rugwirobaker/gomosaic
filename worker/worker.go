3d79648b9537222736ef7c0153d4cb70e611b58c
rugwirobaker <rugwirobaker@gmail.com>
2018-05-31 11:36:59 +0200

    *****

--- worker/worker.go
+++ worker/worker.go
@@ -48,6 +48,7 @@ func AverageColor(img image.Image) [3]float64 {
 
 //LoadTileDB takes in image source and returns a hash table with
 //the image names as the key and the average color of each as the value
+//this function takes in an image source: local directory or a bucket.
 func LoadTileDB() map[string][3]float64 {
 	return map[string][3]float64{}
 }
