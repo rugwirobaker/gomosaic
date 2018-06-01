c1765238c11ab055b991d5d19581addb0ed97a9f
rugwirobaker <rugwirobaker@gmail.com>
2018-06-01 16:36:11 +0200

    ADDED: firebase init

--- engine/gae.go
+++ engine/gae.go
@@ -1,12 +1,27 @@
 package engine
 
+import (
+	"context"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+//App ...
+type App struct{}
+
 //init initializes google app engine appliation instance.
 // it is the entry point of the application.
 func init() {}
 
-// initDB initializes the firebase realtime db using the firebase sdk
-func initDB() {}
+func initFirebase() *firebase.App {
+	opt := option.WithCredentialsFile("path/to/serviceAccountKey.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+	return app
+}
 
-//initStorage initializes a google cloud storage client instance using
-//the gcloud sdk
-func initStorage() {}
+func initFirestore() {}
