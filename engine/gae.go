a089122b66345211e90c00beeee707e57aaed2b5
rugwirobaker <rugwirobaker@gmail.com>
2018-06-01 13:39:29 +0200

    ADDED: gae: app.yml

---
+++ engine/gae.go
@@ -0,0 +1,12 @@
+package engine
+
+//init initializes google app engine appliation instance.
+// it is the entry point of the application.
+func init() {}
+
+// initDB initializes the firebase realtime db using the firebase sdk
+func initDB() {}
+
+//initStorage initializes a google cloud storage client instance using
+//the gcloud sdk
+func initStorage() {}
